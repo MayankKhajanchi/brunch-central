ece27453c51e176209aa021e90b8bfe85c47a516
Mayank Khajanchi <mayankkhajnchi@gmail.com>
2019-05-09 11:54:49 +0530

    changes to restaurant page

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 function initMap() {
 	var home = {lat: 26.174, lng: 91.734};
 	var map = new google.maps.Map(
-		document.getElementById('map'), {zoom: 12, center: home, 
-  		mapTypeControl: false,
-  		streetViewControl: false,
-  		fullscreenControl: false,
-  		gestureHandling: 'cooperative'
-  	});
+		document.getElementById('map'), 
+		{	
+			zoom: 12, 
+			center: home, 
+			mapTypeControl: false,
+			streetViewControl: false,
+			fullscreenControl: false,
+			gestureHandling: 'cooperative'
+		});
 	var marker = new google.maps.Marker({position: home, map: map});
 }
 
 function classToggle() {
 	const navs = document.querySelectorAll('.Navbar__Items')
-
 	navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
 }
 
